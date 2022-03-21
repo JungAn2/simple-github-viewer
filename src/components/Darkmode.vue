70aea1ebad7b5f21486688849cf371e4737e82f3
JungAn2 <jungan1991@gmail.com>
2022-03-21 00:00:47 -0400

    Problem with html parsal when using dark mode

--- src/components/Darkmode.vue
+++ src/components/Darkmode.vue
@@ -1,60 +1,10 @@
 <template>
-    <button @click="Initialize" onload="Initialize()">Toggle</button>
+    <button @click="useMode.Initialize()" onload="lightMode()">Toggle</button>
 </template>>
 
 <script setup lang="ts">
-import { AuthStore } from '@/stores/Auth';
+import { modeStore } from '@/stores/modes'
 
-const useAuth = AuthStore()
-
-/**
- * Set class(dark-mode/dark-mode-transparent) to the body and all child of body
- * 
- * @param body Main element of DOM
- * @param bool Boolean for transparent background
- */
-const recursiveSetDark = (body: any, bool:boolean) => {
-    !bool? body.classList.add("dark-mode"): body.classList.add("dark-mode-transparent")
-    if (body.childElementCount > 0) {
-        for (let i = 0; i < body?.childElementCount; i++) {
-            recursiveSetDark(body.children[i], bool)
-        }
-    }
-}
-
-/**
- * Remove class (dark-mode/dark-mode-transparent) from the body and all child of body
- * 
- * @param body Main element of DOM
- * @param bool Boolean for transparent background
- */
-const recursiveRemoveDark = (body: any, bool: boolean) => {
-    !bool? body.classList.remove("dark-mode"): body.classList.remove("dark-mode-transparent")
-    if (body.childElementCount > 0) {
-        for (let i = 0; i < body?.childElementCount; i++) {
-            recursiveRemoveDark(body.children[i], bool)
-        }
-    }
-}
-
-/**
- *  Toggle of dark mode
- */
-const Initialize = () => {
-    //Main element that wraps around the whole layout
-    const doc = document.getElementById("main")
-
-    if (useAuth.getDarkmode) {
-        recursiveRemoveDark(doc, false)
-        useAuth.setDarkmode(false)
-    }
-    else {
-        recursiveSetDark(doc, false)
-        useAuth.setDarkmode(true)
-    }
-
-}
-
-
+const useMode = modeStore()
 
 </script>
---
+++ src/stores/modes.ts
@@ -0,0 +1,60 @@
+import { defineStore } from "pinia";
+import { AuthStore } from "./Auth";
+
+export const modeStore = defineStore("modeStore", {
+    state: () => ({
+        useAuth: AuthStore()
+    }),
+    getters: {
+
+    },
+    actions: {
+        /**
+         * Set class(dark-mode/dark-mode-transparent) to the body and all child of body
+         * 
+         * @param body Main element of DOM
+         * @param bool Boolean for transparent background
+         */
+        recursiveSetDark(body: any) {
+            body.classList.add("dark-mode")
+            if (body.childElementCount > 0) {
+                for (let i = 0; i < body?.childElementCount; i++) {
+                    this.recursiveSetDark(body.children[i])
+                }
+            }
+        },
+
+        /**
+         * Remove class (dark-mode/dark-mode-transparent) from the body and all child of body
+         * 
+         * @param body Main element of DOM
+         * @param bool Boolean for transparent background
+         */
+        recursiveRemoveDark(body: any) {
+            body.classList.remove("dark-mode")
+            if (body.childElementCount > 0) {
+                for (let i = 0; i < body?.childElementCount; i++) {
+                    this.recursiveRemoveDark(body.children[i])
+                }
+            }
+        },
+
+        /**
+         *  Toggle of dark mode
+         */
+        Initialize() {
+            //Main element that wraps around the whole layout
+            const doc = document.getElementById("homeView")
+
+            if (this.useAuth.getDarkmode) {
+                this.recursiveRemoveDark(doc)
+                this.useAuth.setDarkmode(false)
+            }
+            else {
+                this.recursiveSetDark(doc)
+                this.useAuth.setDarkmode(true)
+            }
+
+        },
+    }
+})
