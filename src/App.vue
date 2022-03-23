2933250c85331ed5a321addd9535df60ef1bc1be
JungAn2 <jungan1991@gmail.com>
2022-03-23 15:01:27 -0400

    Changed baseURL to match github

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,4 @@ import { RouterLink, RouterView } from "vue-router";
 
 <style>
 @import "@/assets/base.css";
-
-#app{
-  margin:0;
-  padding: 0;
-}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/simple-github-viewer/',
   plugins: [vue()],
   resolve: {
     alias: {
